2b2de61730a23fb1dc4d3efa7462f2b63ce16d9f
Maciej Buczyński <kadlubken5@gmail.com>
2017-07-23 14:30:23 +0200

    Added base for extending the system to multiple colors Added checkbox and slider for continuous update

--- src/ant.py
+++ src/ant.py
@@ -1,12 +1,13 @@
 import tkinter as tk
+from tkinter import messagebox
 import numpy as np
 from PIL import Image, ImageTk
-
+from tkinter.colorchooser import *
 
 class ClassicAnt:
 
     def __init__(self, size):
-        self.board = np.full((size, size), False, dtype=bool)  # True=white
+        self.board = np.full((size, size), False, dtype=bool)  # False=white
         self.board_size = size
         self.direction = 0                                  # 0,1,2,3- right,up,left,down
         self.position = [size//2]*2
@@ -51,26 +52,69 @@ class AntGUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.state("zoomed")
-        self.image_label = tk.Label(text="Board", compound="top")
-        self.image_label.pack(side="top", expand=True, padx=8, pady=8)
 
-        self.colors = [(255, 255, 255), (255, 0, 0)]
+        self.directions = []
+        self._colors = []
+        self.color_buttons = []
+        self.repeat_step = tk.IntVar()
+        self.repeat_delay = tk.IntVar()
+
+        self.colors = [(255, 255, 255), (0, 0, 0)]
         self.ant = ClassicAnt(1)
         self.current_image = tk.PhotoImage(translate_array_to_image(self.ant.board, self.colors))
         self.title("Langton's Ant")
         self.geometry("800x600")
 
-        self.single_step_button = tk.Button(text="Single step", command=self.single_step)
-        self.multi_step_button = tk.Button(text="Multi step", command=self.multi_step)
-        self.step_number = tk.Entry(text="Number of steps")
-        self.reset_button = tk.Button(text="Reset", command=lambda: reset(self))
-        # self.anim_speed_button = tk.Scale(self.root, from_=1000, to=10000, label="Animation duration(ms)")
+        self.image_label = tk.Label(text="Board", compound="top")
+        self.options_frame = tk.Frame(self, borderwidth=1)
+        self.top_options_frame = tk.Frame(self.options_frame, borderwidth=1)
+        self.bottom_options_frame = tk.Frame(self.options_frame, relief=tk.RAISED, borderwidth=1)
 
-        self.single_step_button.pack(side="left")
-        self.multi_step_button.pack(side="left")
-        self.step_number.pack(side="left")
-        self.reset_button.pack(side="left")
+        self.single_step_button = tk.Button(self.bottom_options_frame, text="Single step", command=self.single_step, font=("Courier", 44))
+        self.multi_step_button = tk.Button(self.bottom_options_frame, text="Multi step", command=self.multi_step, font=("Courier", 44))
+        self.step_number = tk.Entry(self.bottom_options_frame, width=5, font=("Courier", 44), justify=tk.CENTER)
+        self.repeat_checkbox = tk.Checkbutton(self.bottom_options_frame, variable=self.repeat_step, command=self.continuous_step, text="Step continuously?")
+        self.reset_button = tk.Button(self.bottom_options_frame, text="Reset", command=lambda: reset(self), font=("Courier", 44))
+        self.repeat_delay_slider = tk.Scale(self.bottom_options_frame, from_=50, to=1000, orient=tk.HORIZONTAL, variable=self.repeat_delay)
+
+        self.color_label = tk.Label(self.top_options_frame, text="Number of colors", font=("Courier", 44))
+        self.color_number_entry = tk.Entry(self.top_options_frame, width=1, font=("Courier", 44), justify=tk.CENTER)
+
+        self.bottom_options_frame.columnconfigure(0, pad=3)
+        self.bottom_options_frame.columnconfigure(1, pad=3)
+        self.bottom_options_frame.columnconfigure(2, pad=3)
+        self.bottom_options_frame.columnconfigure(3, pad=3)
+        self.bottom_options_frame.columnconfigure(4, pad=3)
+
+        self.bottom_options_frame.rowconfigure(0, pad=3)
+        self.bottom_options_frame.rowconfigure(1, pad=3)
+        self.bottom_options_frame.rowconfigure(2, pad=3)
+        self.bottom_options_frame.rowconfigure(3, pad=3)
+
+        self.top_options_frame.columnconfigure(0, pad=30)
+        self.top_options_frame.columnconfigure(1, pad=30)
+        self.top_options_frame.columnconfigure(2, pad=3)
+
+        self.top_options_frame.rowconfigure(0, pad=20)
+        self.top_options_frame.rowconfigure(1, pad=3)
+
+        self.options_frame.pack(side="left", expand=True)
+        self.bottom_options_frame.pack(side="bottom")
+        self.top_options_frame.pack(side="top")
+        self.image_label.pack(side="left", expand=True, padx=8, pady=8)
+
+        self.single_step_button.grid(row=0, column=0, columnspan=4)
+        self.multi_step_button.grid(row=1, column=0)
+        self.step_number.grid(row=1, column=1)
+        self.repeat_checkbox.grid(row=2, column=0)
+        self.repeat_delay_slider.grid(row=2, column=1, columnspan=4)
+        self.reset_button.grid(row=3, columnspan=5)
         self.update_image()
+
+        self.color_label.grid(row=0, column=0, columnspan=1)
+        self.color_number_entry.grid(row=0, column=2)
+
+        self.color_number_entry.bind("<Return>", lambda x: self.recreate_color_options())
 
     def single_step(self):
         self.ant.make_move()
@@ -83,7 +127,40 @@ class AntGUI(tk.Tk):
 
     def update_image(self):
         self.current_image = ImageTk.PhotoImage(translate_array_to_image(self.ant.board, self.colors).resize((900, 900)))
-        self.image_label.configure(image=self.current_image, text="Iteration %d" % self.ant.iteration)
+        self.image_label.configure(image=self.current_image, text="Iteration {0}\nimage size = {1}x{1}".format(self.ant.iteration, self.ant.board_size))
+
+    def continuous_step(self):
+        if not self.repeat_step.get():
+            return
+
+        self.single_step()
+        self.after(self.repeat_delay.get(), self.continuous_step)
+
+    def recreate_color_options(self):
+        print("Recreating")
+
+        for child in self.top_options_frame.winfo_children():
+            if child != self.color_label and child != self.color_number_entry:
+                child.destroy()
+
+        number_of_colors = int(self.color_number_entry.get())
+        self.directions = [tk.IntVar() for x in range(number_of_colors)]
+        self._colors = ["white" for x in range(number_of_colors)]
+        self.color_buttons = []
+        set_colors_array = [lambda: self.set_color(x) for x in range(number_of_colors)]
+        for num in range(number_of_colors):
+            self.top_options_frame.rowconfigure(1+num, pad=3)
+            label = tk.Label(self.top_options_frame, text="Color #%d" % (num+1), font=("Courier", 44))
+            action = tk.Checkbutton(self.top_options_frame, variable=self.directions[num])
+            self.color_buttons.append(tk.Button(self.top_options_frame, width=5, height=2, name=str(num), command=set_colors_array[num]))
+            label.grid(row=num+1, column=0)
+            action.grid(row=num+1, column=1)
+            self.color_buttons[num].grid(row=num+1, column=2)
+
+    def set_color(self, button_number):
+        color = askcolor()
+        self.color_buttons[button_number].configure(bg=color[1])
+        self._colors[button_number] = color[1]
 
 
 def translate_array_to_image(array, colors):
